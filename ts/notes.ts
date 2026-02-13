9a7167e605847443d65359246bef39166d366843
Recklore <amancvm@gmail.com>
2026-02-13 15:20:47 +0530

    started typescript

---
+++ ts/notes.ts
@@ -0,0 +1,7 @@
+// tsc app.ts --target es2016 -> converts/compiles the ts code into js (version can be specified by target flag) code and makea a new file
+// tsc --init -> creates a typescript config file
+// tsc --watch -> converts the ts code to js as soon as you write
+
+
+// let a:number = 10; -> string, boolean, null
+// a ="aman" -> this will give error as a is of number type, but it will still create and js file 

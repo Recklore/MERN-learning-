e3346757fe76e9fe1aa5880d6ad86a4f5c011fb3
Recklore <amancvm@gmail.com>
2026-03-12 22:51:15 +0530

    react intro

---
+++ react/notes.js
@@ -0,0 +1,12 @@
+// // REACT -> it is an js library/framework used mainly to build single page webapps
+// CDN links:
+// <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script> -> react
+// <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script> -> react DOM
+// 
+// why are these two different packages? -> because in mobile apps react-native is used and only react package is used there and react-DOM is not requried there.
+
+// const element = React.createElement("h1", {}, "children")
+// const reactRoot = ReactDOM.createRoot(document.getElementById("root")) -> creates a react container for root element (access of rendering comes to this react container)
+// reactRoot.render(element)
+
+// static data is stored in CDNs and the dynamic data is stored in the actual servers of the organisation

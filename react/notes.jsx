d3062cdeacec535b958cdecaf9c5e575c7a73710
Recklore <amancvm@gmail.com>
2026-08-06 12:02:30 +0530

    useState hook

--- react/notes.jsx
+++ react/notes.jsx
@@ -21,9 +21,9 @@
 // npx parcel index.html
 // npx parcel build index.html -> create a folder named "dist" which has the optimized code files that are for production, (it also creates a mapping files that can be used to redo this optimzation and get original js files form the optimized fiels)
 
-// package-lock.json vs package.json -> firstly package-lock is checked first for installing packages and it has exact versions while package.json is used when the lock file is missing and package.json file doesnot have exact version but it can give value of the major version
+// package-lock.json vs package.json -> firstly package-lock is checked first for installing packages and it has exact versions while package.json is used when the lock file is missing and package.json file does not have exact version but it can give value of the major version
 
-// // Metlify for deployment
+// // Netlify for deployment
 
 // // JSX (javascript XML) -> .jsx can have both html and js code together (jsx is not a part of react, babel converts this jsx code in to react code)
 // const name = "some js variable"
@@ -40,3 +40,10 @@
 
 // User defined tags in jsx should have the first letter as capital (eg: <Greet/>)
 // Greet is a js funciton that returns some html, now if need to pass some arguments in this funciton the we can do it using simple html format: <Greet/ key="valuse"> and can be accessed using props: funciton Greet(props) {console.log(props.key)}
+
+// Hooks in React
+// usestate hook: returns an array, with first value the value passed in the fucntion call and the second is a function that would be used to set the value of the variable in the dom whenever it is updated
+// import {useState} from "react"
+// let [count, setCount] = useState(0)
+// count += 1
+// setCount(count)
